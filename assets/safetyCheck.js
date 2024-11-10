9b42b5acbb7742eec37d33495b25425d2808d9f2
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2024-11-10 11:30:34 -0500

    Delete assets/safetyCheck.js

--- assets/safetyCheck.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const originalOpen = window.open;
-    console.log("safteyCheck.js loaded!")
-    window.open = function() {
-        alert("Something you did just caused another tab to open, but luckily I am smart and I made this safety check thing so it like didn't open it.");
-        
-        return undefined;
-    };
-})();
