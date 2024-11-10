8640f06faa0315f8e5f81d4b48bcf3d76e7afd86
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2024-11-10 11:19:38 -0500

    Update safetyCheck_iframe.js

--- assets/safetyCheck_iframe.js
+++ assets/safetyCheck_iframe.js
@@ -1,14 +1,12 @@
 window.onload = function() {
             const iframe = document.getElementById('iframe');
             console.log("safety check 2 loaded iframe ohio SIGMA")
-            // Listen for changes inside the iframe's content
             iframe.onload = function() {
                 const iframeWindow = iframe.contentWindow;
 
-                // Override window.open() within the iframe
                 iframeWindow.open = function(url) {
                     alert("BRuh");
-                    iframe.src = url;  // Change the iframe's src instead of opening a new window
+                    iframe.src = url;  
                 };
             };
         };
