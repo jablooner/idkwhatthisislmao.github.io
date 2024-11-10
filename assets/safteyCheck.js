28ba4aca98b421b3785c35fc97c980f871b7bc7d
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2024-11-10 11:04:37 -0500

    Update safteyCheck.js

--- assets/safteyCheck.js
+++ assets/safteyCheck.js
@@ -1,6 +1,6 @@
 (function() {
     const originalOpen = window.open;
-    alert("safteyCheck.js loaded")
+    console.log("safteyCheck.js loaded!")
     window.open = function() {
         alert("Something you did just caused another tab to open, but luckily I am smart and I made this safety check thing so it like didn't open it.");
         
