062fac965b4279d20ac89bd44951b7347dd51cc0
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2024-11-10 10:37:33 -0500

    Update randomizer.js

--- assets/randomizer.js
+++ assets/randomizer.js
@@ -4,9 +4,10 @@ window.onload = function() {
         { title: "Google Docs", favicon: "https://ssl.gstatic.com/docs/documents/images/kix-favicon-2023q4.ico" },
         { title: "Home - Google Drive", favicon: "https://ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png" },
         { title: "Home", favicon: "https://www.gstatic.com/classroom/ic_product_classroom_144.png" },
-        { title: "Physical Science 24-25", favicon: "https://www.gstatic.com/classroom/ic_product_classroom_144.png" },
+        { title: "Google Slides", favicon: "https://ssl.gstatic.com/docs/presentations/images/favicon-2023q4.ico" },
         { title: "Outlook", favicon: "https://res.public.onecdn.static.microsoft/owamail/20241101001.22/resources/images/favicons/mail-seen.ico" },
         { title: "Untitled document - Google Docs", favicon: "https://ssl.gstatic.com/docs/documents/images/kix-favicon-2023q4.ico" },
+        { title: "Family Portal", favicon: "https://familyportal.renweb.com/favicon-32x32.png" },
     ];
 
     // Randomly select an entry from the list
