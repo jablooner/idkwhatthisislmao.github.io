686188f742b37fa7f2fefb8a740455617aba4635
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2024-11-10 10:55:14 -0500

    Add files via upload

---
+++ assets/safteyCheck.js
@@ -0,0 +1,9 @@
+(function() {
+    const originalOpen = window.open;
+
+    window.open = function() {
+        alert("Something you did just caused another tab to open, but luckily I am smart and I made this safety check thing so it like didn't open it.");
+        
+        return undefined;
+    };
+})();
