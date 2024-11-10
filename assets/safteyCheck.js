bc5c5210c5d8591c6e07ff09a7b99b2d55263fa5
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2024-11-10 10:56:53 -0500

    Update safteyCheck.js

--- assets/safteyCheck.js
+++ assets/safteyCheck.js
@@ -2,7 +2,7 @@
     const originalOpen = window.open;
 
     window.open = function() {
-        alert("Something you did just caused another tab to open, but luckily I am smart and I made this safety check thing so it like didn't open it.");
+        alert("Something you did just caused another tab to open, but luckily I am smart and I made this safety check thing so it like didn't open it");
         
         return undefined;
     };
