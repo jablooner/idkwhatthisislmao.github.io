ed021a0f954401c57ab6ff0759434c3b987b3a23
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2024-11-10 10:49:56 -0500

    Update randomizer.js

--- assets/randomizer.js
+++ assets/randomizer.js
@@ -1,5 +1,4 @@
 window.onload = function() {
-    // List of names and corresponding favicons
     var data = [
         { title: "Google Docs", favicon: "https://ssl.gstatic.com/docs/documents/images/kix-favicon-2023q4.ico" },
         { title: "Home - Google Drive", favicon: "https://ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png" },
@@ -10,11 +9,9 @@ window.onload = function() {
         { title: "Family Portal", favicon: "https://familyportal.renweb.com/favicon-32x32.png" },
     ];
 
-    // Randomly select an entry from the list
     var randomIndex = Math.floor(Math.random() * data.length);
     var selected = data[randomIndex];
 
-    // Set the tab title and favicon
     document.title = selected.title;
     document.getElementById("favicon").href = selected.favicon;
 }
