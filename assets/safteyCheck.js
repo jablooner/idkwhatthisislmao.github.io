e823e184db68c60f56e613994eade15aae5c4bf1
fryingpan923 <132401987+idkwhatthisislmao@users.noreply.github.com>
2024-11-10 10:59:59 -0500

    Update safteyCheck.js

--- assets/safteyCheck.js
+++ assets/safteyCheck.js
@@ -1,9 +1,19 @@
 (function() {
-    const originalOpen = window.open;
-
-    window.open = function() {
-        alert("Something you did just caused another tab to open, but luckily I am smart and I made this safety check thing so it like didn't open it");
-        
-        return undefined;
-    };
+    document.addEventListener('click', function(event) {
+        const target = event.target;
+        if (target.tagName === 'A' && target.target === '_blank') {
+            event.preventDefault();
+            alert("Something you did just caused another tab to open, but luckily I am smart and I made this safety check thing so it like didn't open it.");
+        }
+        if (target.tagName === 'BUTTON' || target.tagName === 'A') {
+            const href = target.getAttribute('href');
+            if (href && (href.startsWith('http://') || href.startsWith('https://'))) {
+                const newWindow = window.open(href, '_blank');
+                if (newWindow) {
+                    newWindow.close();
+                    alert("Something you did just caused another tab to open, but luckily I am smart and I made this safety check thing so it like didn't open it.");
+                }
+            }
+        }
+    });
 })();
